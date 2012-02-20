634c74c8c8cd5a278efed6f43fae347954009360
Dan Gebhardt <dan@cerebris.com>
2012-02-20 16:52:40 -0500

    improved DS.Store.loadAll()

--- app/assets/javascripts/lib/ember-data-extensions.js
+++ app/assets/javascripts/lib/ember-data-extensions.js
@@ -1,19 +1,13 @@
 DS.Store.reopen({
   loadAll: function(type, data) {
+    this.loadMany(type, data);
+
     var array = DS.ModelArray.create({ type: type, content: Em.A([]), store: this });
     this.registerModelArray(array, type);
 
-    this.loadArray(type, data);
+    var typeMap = this.typeMapFor(type);
+    typeMap.findAllCache = array;
 
     return array;
-  },
-
-  loadArray: function(type, array) {
-    // TODO: Why is it necessary to build a separate array of ids? Perhaps this logic could be included in loadMany()?
-    var ids = [];
-    for (var i = 0; i < array.length; i++) {
-      ids.push(array[i].id);
-    }
-    this.loadMany(type, ids, array);
   }
 });
