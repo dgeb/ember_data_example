b0cb33892670d611df28f32a3bdefa95eebd47e2
Dan Gebhardt <dan@cerebris.com>
2012-09-13 13:02:49 -0400

    contacts are now sorted

--- app/assets/javascripts/app/controllers/contacts_controller.js
+++ app/assets/javascripts/app/controllers/contacts_controller.js
@@ -1,4 +1,6 @@
 App.ContactsController = Em.ArrayController.extend({
+  sortProperties: ['lastName', 'firstName'],
+
   init: function() {
     this.set('content', App.store.findAll(App.Contact));
   }
