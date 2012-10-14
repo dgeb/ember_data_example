13c9fcc2f5bbd105d151d57f3bbc014c1349acd1
Dan Gebhardt <dan@cerebris.com>
2012-10-14 14:03:19 -0400

    jquery 1.8.2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,7 +4,7 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-//= require vendor/jquery-1.7.2.min
+//= require vendor/jquery-1.8.2
 //= require vendor/handlebars
 //= require vendor/ember
 //= require vendor/ember-data
