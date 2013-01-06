3e67cf086025f20215842fff7b60d0ceb5efd2ca
Dan Gebhardt <dan@cerebris.com>
2013-01-06 14:53:45 -0500

    converted Bootstrap.ModalPane to simple window.confirm

--- app/assets/javascripts/controllers/contact_controller.js
+++ app/assets/javascripts/controllers/contact_controller.js
@@ -1,24 +1,10 @@
 App.ContactController = Em.ObjectController.extend({
   destroyRecord: function() {
-    var that = this;
-    Bootstrap.ModalPane.popup({
-      heading: 'Delete Contact',
-      message: "Yo sure, wanna delete that one?",
-      primary: "Delete",
-      secondary: "Cancel",
-      callback: function(options) {
-        if(options.primary) {
-          that.get('content').deleteRecord();
-          that.get('store').commit();
-          that.get('target.router').transitionTo('contactsIndex');
-        }
-      }
-    });
+    if (window.confirm("Are you sure you want to delete this contact?")) {
+      this.get('content').deleteRecord();
+      this.get('store').commit();
+      this.get('target.router').transitionTo('contactsIndex');
+    }
   }
-//  destroyRecord: function() {
-//    
-//    this.get('store').commit();
-//    App.router.transitionTo('contacts.index');
-//  }
 });
 
