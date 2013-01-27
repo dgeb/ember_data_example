5cb93df20b4f619b61b5b4d7e3289224ca67c9b8
Dan Gebhardt <dan@cerebris.com>
2013-01-27 18:38:53 -0500

    dirtyRecordsForHasManyChange() workaround to avoid calling _dirtyTree()

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -19,11 +19,10 @@ App.Adapter = DS.RESTAdapter.extend({
         relationship.changeType === 'remove') {
 
       // TODO: understand why the default call to _dirtyTree() is causing problems
-      // WORKAROUND: instead of calling _dirtyTree(), just dirty the parent record
+      // WORKAROUND: instead of calling _dirtyTree(), just dirty the parent and child records
       dirtySet.add(record);
-
-      // var childRecord = relationship.store.recordForReference(relationship.childReference);
-      // dirtySet.add(childRecord);
+      var childRecord = relationship.store.recordForReference(relationship.childReference);
+      dirtySet.add(childRecord);
 
     } else {
       this._super.apply(this, arguments);
