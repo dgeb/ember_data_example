5a9db37a25cf97eeba82a2959b8577fdd0b109fc
Dan Gebhardt <dan@cerebris.com>
2013-05-15 10:26:23 -0400

    Filter contacts to exclude new ones
    [Fixes #41]

--- app/assets/javascripts/routes/contacts_route.js
+++ app/assets/javascripts/routes/contacts_route.js
@@ -1,5 +1,11 @@
 App.ContactsRoute = Ember.Route.extend({
   model: function() {
-    return App.Contact.find();
+    // request all contacts from adapter
+    App.Contact.find();
+
+    // filter contacts to exclude new ones
+    return App.Contact.filter(function(contact) {
+      return !contact.get('isNew');
+    });
   }
 });
