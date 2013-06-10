ea54aca88d548467699dcde7384179a27917c28a
Dan Gebhardt <dan@cerebris.com>
2013-06-10 16:01:57 -0400

    Add `lookupController` to test_utils

--- test/javascripts/test_utils.js
+++ test/javascripts/test_utils.js
@@ -1,10 +1,14 @@
 var lookupStore = function() {
   return App.__container__.lookup('store:main');
-}
+};
 
 var lookupRouter = function() {
   return App.__container__.lookup('router:main');
-}
+};
+
+var lookupController = function(controllerName, options) {
+  return App.__container__.lookup('controller:' + controllerName, options);
+};
 
 var appendView = function(view) {
   Ember.run(function() {
