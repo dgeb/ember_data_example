3b6b44160760367ab94df822e31b6dafcd61cc7f
Dan Gebhardt <dan@cerebris.com>
2013-06-13 07:13:22 -0400

    Move App defer/advance readiness code to be exclusively in test_helper.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -9,7 +9,3 @@
 //= require_tree ./routes
 
 App = Em.Application.create({LOG_TRANSITIONS: true});
-
-// Defer App readiness until it should be advanced for either
-// testing or production.
-App.deferReadiness();
--- test/javascripts/test_helper.js
+++ test/javascripts/test_helper.js
@@ -3,27 +3,28 @@
 //= require application
 //= require test_utils
 
-// Prevent automatic scheduling of runloops. For tests, we
-// want to have complete control of runloops.
-Ember.testing = true;
-
-// Prevent the router from manipulating the browser's URL.
-App.Router.reopen({location: 'none'});
+// Sinon fake server
+var server;
 
 // Stub out Konacha.reset()
 Konacha.reset = Ember.K;
 
-// Sinon fake server
-var server;
+// Prevent automatic scheduling of runloops. For tests, we
+// want to have complete control of runloops.
+Ember.testing = true;
+
+// Defer App readiness (it will be advanced in each test below)
+App.deferReadiness();
+
+// Prevent the router from manipulating the browser's URL.
+App.Router.reopen({location: 'none'});
 
 beforeEach(function(done) {
   // Fake XHR
   server = sinon.fakeServer.create();
 
   Ember.run(function() {
-    // Advance App readiness, which was deferred when the app
-    // was created. This needs to be done here, after each iframe has been
-    // setup, instead of in a global `before`.
+    // Advance App readiness, which was deferred above.
     App.advanceReadiness();
 
     // Setup is complete when the App readiness promise resolves
