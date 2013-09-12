60611e8a60e38792d5a80803395d021e6fff5dfc
Kunal Kamble <kunal@jugnoo.com>
2013-09-12 14:58:20 -0400

    Updating user email address, using registerBoundHelper instead of registerHelper.

--- app/assets/javascripts/helpers/forms.js
+++ app/assets/javascripts/helpers/forms.js
@@ -2,8 +2,7 @@ Handlebars.registerHelper('submitButton', function(text) {
   return new Handlebars.SafeString('<button type="submit" class="btn btn-primary">' + text + '</button>');
 });
 
-Handlebars.registerHelper('mailto', function(field) {
-  var address = this.get(field);
+Ember.Handlebars.registerBoundHelper('mailto', function(address) {
   if (address) {
     return new Handlebars.SafeString('<a href="mailto: ' + address + '" />' + address + '</a>');
   }
